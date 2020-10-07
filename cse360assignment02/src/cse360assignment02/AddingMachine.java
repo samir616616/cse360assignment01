baef214e1b483231f870d33eb51eab43acb2cee4
samir616616 <72422265+samir616616@users.noreply.github.com>
2020-10-06 20:44:57 -0700

    Initial version of project.

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,29 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	
+	public AddingMachine () {
+		total = 0; // not needed -included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		//code here for adding function
+	}
+	
+	public void subtract (int value) {
+		//code here for subtracting function
+	}
+	
+	public String toString () {
+		return ""; //code to turn into string
+	}
+	
+	public void clear() {
+	} //clears inputs
+
+}
